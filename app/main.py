b96757cb108522c7b3fee64ae47065b7a1e361aa
land-pack <landpack@sina.com>
2019-09-27 06:25:16 +1000

    add cart

--- app/main.py
+++ app/main.py
@@ -8,5 +8,9 @@ app = Flask(__name__, static_url_path="")
 def index():
     return render_template("index.html")
 
+@app.route("/cart")
+def cart():
+    return render_template("cart.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
